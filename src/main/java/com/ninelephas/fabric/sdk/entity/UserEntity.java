487c8843ff5d72fa3d97406daf410365f7f9da40
邹伟 <zouwei@9elephas.com>
2017-04-24 14:17:01 +0800

    添加用户实体注释

--- src/main/java/com/ninelephas/fabric/sdk/entity/UserEntity.java
+++ src/main/java/com/ninelephas/fabric/sdk/entity/UserEntity.java
@@ -9,25 +9,40 @@ import java.util.ArrayList;
 
 /**
  * Created by zouwei on 2017/4/18.
+ * 必须要实现fabric的User接口
  */
 public class UserEntity implements User, Serializable {
 
     private static final long serialVersionUID = 8077132186383604355L;
-
+    /**
+     * 用户名，唯一确定一个用户，相当于id
+     */
     private String name;
-
+    /**
+     * 用户角色
+     */
     private ArrayList<String> roles;
-
+    /**
+     *
+     */
     private String account;
-
+    /**
+     * 部门
+     */
     private String affiliation;
-
+    /**
+     * 注册后的用户该字段不能为空
+     */
     private Enrollment enrollment;
 
     private String mspID;
-
+    /**
+     * 注册后的用户该字段不能为空
+     */
     private String secret;
-
+    /**
+     * 指定该用户所属管理员，该管理员必须已经存在
+     */
     private UserEntity admin;
 
     public UserEntity(){
