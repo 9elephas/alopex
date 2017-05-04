086064dfe7987ecae53174ac1a82ae152bdbf409
邹伟 <zouwei@9elephas.com>
2017-05-04 16:29:09 +0800

    用户注册渐入缓存

--- src/main/java/com/ninelephas/alopex/service/fabric/UserService.java
+++ src/main/java/com/ninelephas/alopex/service/fabric/UserService.java
@@ -8,6 +8,7 @@
 package com.ninelephas.alopex.service.fabric;
 
 import com.ninelephas.alopex.controller.pojo.FabricUser;
+import com.ninelephas.alopex.service.ServiceException;
 import com.ninelephas.common.configer.ConfigHelper;
 import com.ninelephas.fabric.sdk.entity.UserEntity;
 import com.ninelephas.fabric.sdk.service.impl.ClientService;
@@ -15,7 +16,12 @@ import com.ninelephas.fabric.sdk.service.impl.ClientServiceImpl;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.configuration2.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * 阿斯蒂芬
@@ -34,20 +40,32 @@ public class UserService {
     @Autowired
     private ClientService clientService;
 
-    public void register(FabricUser fabricUser) throws Exception {
-        Configuration configuration = ConfigHelper.getConfig();
-        //CA url
-        //String caUrl = configuration.getString("Fabric.CA.URL");
-        String secret = configuration.getString("Fabric.Admin.Secret");
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
-        //clientService = new ClientServiceImpl(caUrl);
-        //创建用户
-        String mspID = "Org1MSP";
-        UserEntity admin = new UserEntity(fabricUser.getUserName(), mspID);
-        admin.setSecret(secret);
-        UserEntity user = new UserEntity("user1", mspID);
-        user.setAffiliation("org1.department1");
-        user.setAdmin(admin);
-        boolean res = clientService.regist(user);
+
+    public void register(FabricUser fabricUser) throws Exception {
+        String userName = fabricUser.getUserName();
+        Boolean hasUserName = redisTemplate.hasKey(userName);
+        if (hasUserName) {
+            throw new ServiceException("该用户已注册");
+        }else{
+            Configuration configuration = ConfigHelper.getConfig();
+            //CA url
+            //String caUrl = configuration.getString("Fabric.CA.URL");
+            String secret = configuration.getString("Fabric.Admin.Secret");
+
+            //clientService = new ClientServiceImpl(caUrl);
+            //创建用户
+            String mspID = "Org1MSP";
+            UserEntity admin = new UserEntity("admin", mspID);
+            admin.setSecret(secret);
+            UserEntity user = new UserEntity(userName, mspID);
+            user.setAffiliation("org1.department1");
+            user.setAdmin(admin);
+            if(clientService.regist(user)){
+                redisTemplate.boundSetOps(userName).add(user);
+            }
+        }
     }
 }
